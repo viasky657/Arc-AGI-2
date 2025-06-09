8a0985d6bc3acf44631a41ea25d120a7b363f695
viasky657 <89562411+viasky657@users.noreply.github.com>
2025-06-09 14:41:26 +0000

    Automated commit of all changes

--- contineous-thought-machines/models/constants.py
+++ contineous-thought-machines/models/constants.py
@@ -1,4 +1,11 @@
-VALID_NEURON_SELECT_TYPES = ['first-last', 'random', 'random-pairing']
+VALID_NEURON_SELECT_TYPES = [
+    'first-last', 'random', 'random-pairing',  # Legacy
+    # Biologically-inspired types
+    'bio_hebbian', 'bio_plasticity', 'bio_competitive', 'bio_homeostatic',
+    'bio_evolutionary', 'bio_stdp', 'bio_criticality', 'bio_multi_objective',
+    # Hybrid approaches
+    'adaptive_random', 'performance_guided', 'task_aware'
+]
 
 VALID_BACKBONE_TYPES = [
     f'resnet{depth}-{i}' for depth in [18, 34, 50, 101, 152] for i in range(1, 5)
